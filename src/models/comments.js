bca40dd5fcccff4c67f83bf3983921afe12d809a
leny <info@flatland.be>
2017-04-17 17:23:43 +0200

    Add basic model (and blind) layer for comments

---
+++ src/models/comments.js
@@ -0,0 +1,43 @@
+/* hepl-ria/vitfoud-server
+ *
+ * ~/models/comments.js - Comments Model
+ *
+ * coded by leny@flatLand!
+ * started at 17/04/2017
+ */
+
+const ONE_DAY = 86400000,
+    oCommentsStore = new Map();
+
+let fGet, fSet;
+
+fGet = function( sSlug ) {
+    let aComments = [];
+
+    if ( oCommentsStore.has( sSlug ) && fGet.lastCall < ONE_DAY ) {
+        aComments = oCommentsStore.get( sSlug );
+    }
+
+    fGet.lastCall = Date.now();
+
+    return aComments;
+};
+
+fGet.lastCall = Date.now();
+
+fSet = function( sSlug, oComment ) {
+    let aComments = fGet( sSlug );
+
+    if ( aComments.length >= 100 ) {
+        aComments.shift();
+    }
+
+    aComments.push( oComment );
+
+    oCommentsStore.set( sSlug, aComments );
+};
+
+export {
+    fGet as get,
+    fSet as set,
+};
